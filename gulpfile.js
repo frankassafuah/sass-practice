3b21a2ccc78c84395b7d8b21117bcc4276c6c43b
Frank Assafuah <frankassafuah@gmail.com>
2022-11-16 22:42:49 +0000

    Partials

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const sass = require('gulp-sass')(require('sass'))
 
 //function to compile our sass
 function buildStyles() {
-    return src('index.scss')
+    return src('*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 //watcher function to watch sass file for chnages and automatically update css file when saved
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
