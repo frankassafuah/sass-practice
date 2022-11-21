793b3f03988a0af48354a1f8ae26241e7db4a47e
Frank Assafuah <frankassafuah@gmail.com>
2022-11-21 21:16:25 +0000

    Purging CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 
 //function to compile our sass
 function buildStyles() {
     return src('scss/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 //watcher function to watch sass file for chnages and automatically update css file when saved
 function watchTask() {
-    watch(['scss/**/*.scss'], buildStyles)
+    watch(['scss/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
